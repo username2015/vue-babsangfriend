c60c1bbe3af2d9beceeb7fbeab1e95117254b133
username2015 <l030105@naver.com>
2025-05-20 12:46:42 +0900

    fix nice

--- src/views/DevStory.vue
+++ src/views/DevStory.vue
@@ -56,13 +56,10 @@
       </div>
     </div>
   </div>
-  <Footer />
 </template>
 
 <script setup lang="ts">
 import Header from '../components/Header.vue';
-
-import Footer from '../components/Footer.vue';
 </script>
 
 <style scoped>
--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -37,13 +37,11 @@
       </div>
     </div>
   </div>
-  <Footer />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
 
 const faqs = ref([
   {
